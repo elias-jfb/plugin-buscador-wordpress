9ee8c3da0fe4257267e1e2fd9df303af0d1ae019
Li Wei <121266973+liwei-gif@users.noreply.github.com>
2024-08-28 15:31:35 -0400

    v1

--- simple-search-plugin/simple-search-plugin.php
+++ simple-search-plugin/simple-search-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Simple Search Plugin
-Description: Un plugin sencillo de buscador hecho en HTML y CSS.
+Description: Un plugin sencillo de buscador hecho en HTML y CSS, integrado como un widget de Elementor.
 Version: 1.0
 Author: Tu Nombre
 */
@@ -10,12 +10,185 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Salir si se accede directamente
 }
 
-// Incluir el archivo del shortcode
-require_once plugin_dir_path( __FILE__ ) . 'includes/shortcode.php';
+// Definir la clase del widget
+function simple_search_init() {
+    class Simple_Search_Widget extends \Elementor\Widget_Base {
+        public function get_name() {
+            return 'simple_search';
+        }
 
-// Encolar el estilo CSS
-function simple_search_enqueue_styles() {
-    wp_enqueue_style('simple-search-styles', plugin_dir_url(__FILE__) . 'assets/css/simple-search.css');
+        public function get_title() {
+            return __( 'Simple Search', 'simple-search-plugin' );
+        }
+
+        public function get_icon() {
+            return 'eicon-search';
+        }
+
+        public function get_categories() {
+            return [ 'general' ];
+        }
+
+        protected function _register_controls() {
+            // Sección de contenido
+            $this->start_controls_section(
+                'content_section',
+                [
+                    'label' => __( 'Content', 'simple-search-plugin' ),
+                    'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+                ]
+            );
+
+            $this->add_control(
+                'search_placeholder',
+                [
+                    'label' => __( 'Search Placeholder', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::TEXT,
+                    'default' => __( 'aqui va la direccion', 'simple-search-plugin' ),
+                ]
+            );
+
+            $this->end_controls_section();
+
+            // Sección de estilo
+            $this->start_controls_section(
+                'style_section',
+                [
+                    'label' => __( 'Style', 'simple-search-plugin' ),
+                    'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+                ]
+            );
+
+            // Estilo del formulario
+            $this->add_control(
+                'form_background_color',
+                [
+                    'label' => __( 'Form Background Color', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} .simple-search-form' => 'background-color: {{VALUE}};',
+                    ],
+                ]
+            );
+
+            $this->add_group_control(
+                \Elementor\Group_Control_Border::get_type(),
+                [
+                    'name' => 'form_border',
+                    'label' => __( 'Form Border', 'simple-search-plugin' ),
+                    'selector' => '{{WRAPPER}} .simple-search-form',
+                ]
+            );
+
+            $this->add_control(
+                'form_border_radius',
+                [
+                    'label' => __( 'Form Border Radius', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                    'size_units' => [ 'px', '%' ],
+                    'selectors' => [
+                        '{{WRAPPER}} .simple-search-form' => 'border-radius: {{TOP}} {{RIGHT}} {{BOTTOM}} {{LEFT}};',
+                    ],
+                ]
+            );
+
+            $this->add_control(
+                'form_padding',
+                [
+                    'label' => __( 'Form Padding', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                    'size_units' => [ 'px', '%' ],
+                    'selectors' => [
+                        '{{WRAPPER}} .simple-search-form' => 'padding: {{TOP}} {{RIGHT}} {{BOTTOM}} {{LEFT}};',
+                    ],
+                ]
+            );
+
+            // Estilo del campo de búsqueda
+            $this->add_control(
+                'input_background_color',
+                [
+                    'label' => __( 'Input Background Color', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} .simple-search-field' => 'background-color: {{VALUE}};',
+                    ],
+                ]
+            );
+
+            $this->add_control(
+                'input_text_color',
+                [
+                    'label' => __( 'Input Text Color', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::COLOR,
+                    'selectors' => [
+                        '{{WRAPPER}} .simple-search-field' => 'color: {{VALUE}};',
+                    ],
+                ]
+            );
+
+            $this->add_group_control(
+                \Elementor\Group_Control_Border::get_type(),
+                [
+                    'name' => 'input_border',
+                    'label' => __( 'Input Border', 'simple-search-plugin' ),
+                    'selector' => '{{WRAPPER}} .simple-search-field',
+                ]
+            );
+
+            $this->add_control(
+                'input_border_radius',
+                [
+                    'label' => __( 'Input Border Radius', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                    'size_units' => [ 'px', '%' ],
+                    'selectors' => [
+                        '{{WRAPPER}} .simple-search-field' => 'border-radius: {{TOP}} {{RIGHT}} {{BOTTOM}} {{LEFT}};',
+                    ],
+                ]
+            );
+
+            $this->add_control(
+                'input_padding',
+                [
+                    'label' => __( 'Input Padding', 'simple-search-plugin' ),
+                    'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                    'size_units' => [ 'px', '%' ],
+                    'selectors' => [
+                        '{{WRAPPER}} .simple-search-field' => 'padding: {{TOP}} {{RIGHT}} {{BOTTOM}} {{LEFT}};',
+                    ],
+                ]
+            );
+
+            $this->add_group_control(
+                \Elementor\Group_Control_Typography::get_type(),
+                [
+                    'name' => 'input_typography',
+                    'label' => __( 'Typography', 'simple-search-plugin' ),
+                    'selector' => '{{WRAPPER}} .simple-search-field',
+                ]
+            );
+
+            $this->end_controls_section();
+        }
+
+        protected function render() {
+            $settings = $this->get_settings_for_display();
+            echo '<form role="search" method="get" class="simple-search-form" action="' . home_url( '/' ) . '">';
+            echo '<input type="text" class="simple-search-field" value="' . esc_attr( $settings['search_placeholder'] ) . '" readonly />';
+            echo '</form>';
+        }
+
+        protected function _content_template() {
+            ?>
+            <form role="search" method="get" class="simple-search-form" action="<?php echo home_url( '/' ); ?>">
+                <input type="text" class="simple-search-field" value="{{{ settings.search_placeholder }}}" readonly />
+            </form>
+            <?php
+        }
+    }
+
+    // Registrar el widget con Elementor
+    \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Simple_Search_Widget() );
 }
-add_action('wp_enqueue_scripts', 'simple_search_enqueue_styles');
-?>
+add_action( 'elementor/widgets/register', 'simple_search_init' );
